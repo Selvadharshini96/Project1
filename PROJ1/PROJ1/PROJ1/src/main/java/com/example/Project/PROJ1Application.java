218223034bec1eba7f46c66d3ca5d0c7c07d33d8
Selvadharshini96 <116495927+Selvadharshini96@users.noreply.github.com>
2023-08-02 10:12:20 +0530

    Add files via upload

---
+++ PROJ1/PROJ1/PROJ1/src/main/java/com/example/Project/PROJ1Application.java
@@ -0,0 +1,13 @@
+package com.example.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectK1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectK1Application.class, args);
+	}
+
+}
